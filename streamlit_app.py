d2b99b86194aa0cdd42574574be6723bc2fa73b9
bhanuravipati <bhanuchowdary8989@gmail.com>
2024-10-05 12:16:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,25 @@
 import streamlit as st
+import pandas as pd
+import sklearn
+from sklearn.linear_model import LinearRegression
+import pickle
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+model = pickle.load(open(r"estimator.pkl","rb"))
+
+st.image("inno1.webp")
+start_lat = st.number_input("Enter the start latitude:",)
+start_lang = st.number_input("Enter the start longitude:")
+end_lat = st.number_input("Enter the destination latitude:")
+end_lang = st.number_input("Enter the destination longitude:")
+dist = st.number_input("Enter the distance:")
+density = st.number_input("Enter the density:")
+weather = st.text_input("Enter the weather condition:",value="rainy")
+day  = st.number_input("Enter the day:")
+hour = st.number_input("Enter the hour:")
+
+weather_numerical = (1 if weather == "rainy" else 2 if weather == "foggy" else 3)
+if st.button("Submit"):
+    time = model.predict([[start_lat,start_lang ,end_lat,end_lang,dist,density,weather_numerical,day,hour]])[0]
+    st.write("The Estimated time is",time,'minutes')
+
+
